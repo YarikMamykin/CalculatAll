9cd8f6628b47c25f8d04404bd5f7ea376c7e803c
YarikMamykin <ymamykin@gmail.com>
2024-11-03 15:21:44 +0200

    Focus widget when clicking on it on WorkField

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { defineProps, ref } from 'vue'
 import { widgets } from '@/components/widgets'
 
 const props = defineProps({
   widgetType: { type: String, required: true }
 })
+
+const widgetRef = ref()
+function focus(_: unknown) {
+  widgetRef.value.focus({ preventScroll: true, focusVisible: true })
+}
 </script>
 
 <template>
-  <div class="widget"><component :is="widgets[widgetType]" /></div>
+  <div ref="widgetRef" class="widget" @click="(e) => focus(e)" tabindex="1">
+    <component :is="widgets[widgetType]" />
+  </div>
 </template>
