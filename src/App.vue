5f2dd853a0c3b8233a082a6ae3d6291c8d66967b
YarikMamykin <ymamykin@gmail.com>
2024-11-03 14:25:26 +0200

    Implement WidgetControlPanel component
    WidgetControlPanel component is responsible for providing operations on
    Widget like resizing, moving around WorkField, etc.
    WidgetControlPanel is placed immediately after ControlPanel and is shown
    only when certain Widget is selected.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import ControlPanel from '@/components/ControlPanel.vue'
+import WidgetControlPanel from '@/components/WidgetControlPanel.vue'
 import WorkField from '@/components/WorkField.vue'
 </script>
 
 <template>
   <ControlPanel />
+  <WidgetControlPanel />
   <WorkField />
 </template>
