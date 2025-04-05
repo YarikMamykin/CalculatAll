91397e5b2d31bc607f0c94ff1dfe7bb81d6c93fa
YarikMamykin <ymamykin@gmail.com>
2025-04-05 19:06:53 +0300

    Add placeholders for main components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import ControlPanel from "./components/ControlPanel.vue";
+import WorkField from "./components/WorkField.vue";
 </script>
 
 <template>
+  <control-panel />
+  <work-field />
 </template>
