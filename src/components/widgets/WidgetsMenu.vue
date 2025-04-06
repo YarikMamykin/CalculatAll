bbb4fa9819da9634fc4989fd5c10c4d60c1646b4
YarikMamykin <ymamykin@gmail.com>
2025-04-07 01:09:46 +0300

    Implement WidgetsMenu

---
+++ src/components/widgets/WidgetsMenu.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Headline from "./Headline.vue";
+</script>
+
+<template>
+  <div id="widgets-menu-container">
+    <div id="widgets-menu"><headline title="Widgets" /></div>
+  </div>
+</template>
