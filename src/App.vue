b9abd959462076c5a7a2eeaed7f9d3849ae56d1a
YarikMamykin <ymamykin@gmail.com>
2025-04-08 00:33:58 +0300

    Toggle widgets menu visibility by `add` and `close` buttons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import ControlPanel from "./components/ControlPanel.vue";
 import WorkField from "./components/WorkField.vue";
+import WidgetsMenu from "./components/widgets/WidgetsMenu.vue";
+import { useWidgetsMenuStore } from "./store/widgets_menu";
+
+const widgetsMenuState = useWidgetsMenuStore();
 </script>
 
 <template>
+  <widgets-menu v-show="widgetsMenuState.show" />
   <control-panel />
   <work-field />
 </template>
--- src/components/widgets/WidgetsMenu.vue
+++ src/components/widgets/WidgetsMenu.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import Headline from "./Headline.vue";
+import { useWidgetsMenuStore } from "../../store/widgets_menu";
+
+const widgetsMenuState = useWidgetsMenuStore();
 </script>
 
 <template>
   <div id="widgets-menu-container">
-    <div id="widgets-menu"><headline title="Widgets" /></div>
+    <div id="widgets-menu">
+      <headline title="Widgets" @close="widgetsMenuState.show = false" />
+    </div>
   </div>
 </template>
