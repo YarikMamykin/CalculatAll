978cc4d53025b717bc697c7339c39c97149242c3
YarikMamykin <ymamykin@gmail.com>
2025-04-08 00:29:50 +0300

    Implement Headline component for widgets and menus

---
+++ src/components/widgets/Headline.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import CloseButton from "../buttons/CloseButton.vue";
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  title: { type: String, required: true },
+});
+
+const emit = defineEmits(["close"]);
+</script>
+
+<template>
+  <div class="all-centered headline">
+    <div style="width: 10px" />
+    <label>{{ props.title }}</label>
+    <close-button @click="() => emit('close')">
+      <img class="default-inner-size" src="@/assets/cross-sign.svg" />
+    </close-button>
+  </div>
+</template>
