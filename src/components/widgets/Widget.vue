dd8d883d98a150473d8ee13b85afb78cdd19e9c9
YarikMamykin <ymamykin@gmail.com>
2025-04-13 02:11:56 +0300

    Implement widgets and menu

---
+++ src/components/widgets/Widget.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+import Headline from "./Headline.vue";
+import { widgets } from "./widgets";
+
+const props = defineProps({
+  name: { type: String, required: false },
+  widgetType: { type: String, required: true },
+});
+</script>
+
+<template>
+  <div class="widget">
+    <headline :title="props.name" />
+    <component :is="props.widgetType" />
+  </div>
+</template>
--- src/components/widgets/WidgetsMenu.vue
+++ src/components/widgets/WidgetsMenu.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Headline from "./Headline.vue";
 import { useWidgetsMenuStore } from "../../store/widgets_menu";
+import { widgets } from "./widgets";
+import Widget from "./Widget.vue";
 
 const widgetsMenuState = useWidgetsMenuStore();
 </script>
@@ -9,6 +11,14 @@ const widgetsMenuState = useWidgetsMenuStore();
   <div id="widgets-menu-container">
     <div id="widgets-menu">
       <headline title="Widgets" @close="widgetsMenuState.show = false" />
+      <div class="widgets-preview-container">
+        <widget
+          v-for="(widget, name) of widgets"
+          :key="widget"
+          :widgetType="widget"
+          :name="name"
+        />
+      </div>
     </div>
   </div>
 </template>
