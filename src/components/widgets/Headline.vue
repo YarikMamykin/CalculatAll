2d6bf134306ead5e8ebf0ca917df7045bfc33245
YarikMamykin <ymamykin@gmail.com>
2025-04-13 02:10:51 +0300

    Implement widgets and menu headline

--- src/components/widgets/Headline.vue
+++ src/components/widgets/Headline.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
-import CloseButton from "../buttons/CloseButton.vue";
+import WidgetHeadlineButton from "../buttons/WidgetHeadlineButton.vue";
 import { defineProps, defineEmits } from "vue";
 
 const props = defineProps({
   title: { type: String, required: true },
 });
 
-const emit = defineEmits(["close"]);
+const emit = defineEmits(["close,settings"]);
 </script>
 
 <template>
-  <div class="all-centered headline">
-    <div style="width: 10px" />
-    <label>{{ props.title }}</label>
-    <close-button @click="() => emit('close')">
-      <img class="default-inner-size" src="@/assets/cross-sign.svg" />
-    </close-button>
+  <div class="headline">
+    <label class="headline-title">{{ props.title }}</label>
+    <div class="all-centered headline-buttons">
+      <widget-headline-button @click="() => emit('settings')">
+        <img class="default-inner-size" src="@/assets/gear-sign.svg" />
+      </widget-headline-button>
+      <widget-headline-button @click="() => emit('close')">
+        <img class="default-inner-size" src="@/assets/cross-sign.svg" />
+      </widget-headline-button>
+    </div>
   </div>
 </template>
