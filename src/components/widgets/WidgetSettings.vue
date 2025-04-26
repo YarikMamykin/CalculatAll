1d30c4e7d5b4134e134db22f7d0e731572382ce1
YarikMamykin <ymamykin@gmail.com>
2025-04-26 16:57:26 +0300

    Use settings copy in WidgetSettings component

--- src/components/widgets/WidgetSettings.vue
+++ src/components/widgets/WidgetSettings.vue
@@ -1,26 +1,39 @@
 <script setup lang="ts">
 import { defineProps, defineEmits } from "vue";
+import { useWorkfieldStore } from "../../store/workfield";
+import { WidgetSettings } from "../../store/widget_settings";
 import SettingsHeadline from "./SettingsHeadline.vue";
 
 const props = defineProps({
-  defaultName: { type: String, required: true },
   widgetId: { type: String, required: true },
 });
 
 const emit = defineEmits(["save", "cancel"]);
+
+const workfieldStore = useWorkfieldStore();
+
+const widgetSettings: WidgetSettings =
+  workfieldStore.widgets[props.widgetId].settings;
+
+const settings: WidgetSettings = widgetSettings.clone();
+
+function save() {
+  workfieldStore.updateWidget(props.widgetId, settings);
+  emit("save");
+}
 </script>
 
 <template>
   <div class="widget-settings">
     <settings-headline
       title="Settings"
-      @save="() => emit('save')"
+      @save="save"
       @cancel="() => emit('cancel')"
     />
     <div class="widget-settings-items">
       <div class="widget-settings-item">
         <label>Name</label>
-        <input type="text" :value="props.defaultName" />
+        <input type="text" v-model="settings.name" />
       </div>
       <slot />
     </div>
