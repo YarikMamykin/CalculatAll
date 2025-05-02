ca3d51909eef8a238c54e7c839321128d12d4792
YarikMamykin <ymamykin@gmail.com>
2025-05-02 18:05:01 +0300

    Remove id generation from WidgetPreview

--- src/components/widgets/WidgetPreview.vue
+++ src/components/widgets/WidgetPreview.vue
@@ -3,7 +3,6 @@ import { type Component as AsyncComponent } from "vue";
 import { type PropType, shallowRef } from "vue";
 import { useWorkfieldStore } from "../../store/workfield";
 import { WidgetSettings } from "../../store/widget_settings";
-import { generateWidgetId } from "../widgets/widgets";
 
 const props = defineProps({
   name: { type: String, required: true },
@@ -15,7 +14,6 @@ const workfieldStore = useWorkfieldStore();
 function addWidget(e: Event) {
   e.stopPropagation();
   workfieldStore.addWidget({
-    id: generateWidgetId(),
     settings: new WidgetSettings(props.name),
     widgetType: shallowRef(props.widgetType),
   });
