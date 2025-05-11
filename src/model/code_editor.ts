8a8ee43c5648f700394ec3cd0debee9fd9027e90
YarikMamykin <ymamykin@gmail.com>
2025-05-12 00:11:49 +0300

    Implement code format and line-wrap

--- src/model/code_editor.ts
+++ src/model/code_editor.ts
@@ -5,6 +5,7 @@ import { EditorState, Compartment, Prec } from "@codemirror/state";
 import { javascript } from "@codemirror/lang-javascript";
 import { HighlightStyle, syntaxHighlighting } from "@codemirror/language";
 import { tags } from "@lezer/highlight";
+import { js } from "js-beautify";
 
 export class CodeEditor {
   public view: EditorView | null;
@@ -48,6 +49,7 @@ export class CodeEditor {
       doc: intialValue,
       extensions: [
         basicSetup,
+        EditorView.lineWrapping,
         javascript({ typescript: true }),
         languageCompartment.of(javascript({ typescript: true })),
         themeCompartment.of(customTheme),
@@ -70,6 +72,9 @@ export class CodeEditor {
             // event.stopPropagation();
             return false;
           },
+          blur: (_, __) => {
+            this.format();
+          },
         }),
       ],
     });
@@ -85,10 +90,30 @@ export class CodeEditor {
       state: this.startState,
       parent: this.editorRef.value,
     });
+    this.format();
   }
 
   public unmount(): void {
     this.view?.destroy();
     this.view = null;
+  }
+
+  public format(): void {
+    if (!this.view) {
+      return;
+    }
+    // Get the current code from CodeMirror
+    const code = this.view.state.doc.toString();
+    const formattedCode = js(code);
+    this.view.dispatch({
+      changes: {
+        from: 0,
+        to: this.view.state.doc.length,
+        insert: formattedCode,
+      },
+    });
+
+    // Use pretty-quick to format the code
+    // Note: pretty-quick typically works with files, so we simulate a file-like input
   }
 }
