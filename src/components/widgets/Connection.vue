16e56cfe2152d5692198a39d049351e654188471
YarikMamykin <ymamykin@gmail.com>
2025-05-25 12:18:25 +0300

    Implement Connection component

---
+++ src/components/widgets/Connection.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps({
+  points: { type: String, required: true },
+});
+</script>
+
+<template>
+  <svg>
+    <polyline class="connection" :points="props.points"></polyline>
+  </svg>
+</template>
