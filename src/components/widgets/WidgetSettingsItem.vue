13ddec3cea553b87a87ab2db2c5cecbb32b5d0d6
YarikMamykin <ymamykin@gmail.com>
2025-06-08 00:49:45 +0300

    Implement Widget Settings Item

---
+++ src/components/widgets/WidgetSettingsItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+const props = defineProps({
+  name: { type: String, required: true },
+});
+</script>
+
+<template>
+  <div class="widget-settings-item">
+    <label>{{ props.name }}</label>
+    <slot />
+  </div>
+</template>
